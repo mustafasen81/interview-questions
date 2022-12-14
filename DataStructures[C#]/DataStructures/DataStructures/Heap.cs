057b3ce0e96bdcd4a9729afce68747400ba72957
Mustafa SEN <msen0681@gmail.com>
2022-12-14 17:43:53 +0300

    [DataStructures][C#] make Heap.Remove faster

--- DataStructures[C#]/DataStructures/DataStructures/Heap.cs
+++ DataStructures[C#]/DataStructures/DataStructures/Heap.cs
@@ -3,6 +3,7 @@ namespace DataStructures;
 public class Heap<T> where T : IComparable<T>
 {
     private List<T> storage = new List<T>();
+    private Dictionary<T, List<int>> valueToIndexMap = new Dictionary<T, List<int>>();
 
     private int LeftChild(int root)
     {
@@ -26,6 +27,10 @@ public class Heap<T> where T : IComparable<T>
         var temp = storage[node1];
         storage[node1] = storage[node2];
         storage[node2] = temp;
+        valueToIndexMap[storage[node1]].Add(node1);
+        valueToIndexMap[storage[node2]].Add(node2);
+        valueToIndexMap[storage[node1]].Remove(node2);
+        valueToIndexMap[storage[node2]].Remove(node1);
     }
 
     private void BubbleUp(int node)
@@ -92,6 +97,14 @@ public class Heap<T> where T : IComparable<T>
     public void Insert(T data)
     {
         storage.Add(data);
+        if (valueToIndexMap.ContainsKey(data))
+        {
+            valueToIndexMap[data].Add(storage.Count - 1);
+        }
+        else
+        {
+            valueToIndexMap.Add(data, new List<int>(new int[] { storage.Count - 1 }));
+        }
         BubbleUp(storage.Count - 1);
     }
 
@@ -112,9 +125,9 @@ public class Heap<T> where T : IComparable<T>
         {
             throw new InvalidOperationException("Empty heap.");
         }
-        int index = storage.FindIndex(item => item.CompareTo(data) == 0);
-        if (index >= 0)
+        if (valueToIndexMap.ContainsKey(data))
         {
+            int index = valueToIndexMap[data].First();
             RemoveAt(index);
         }
     }
